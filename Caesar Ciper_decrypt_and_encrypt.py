d4d9e91b65c2fc50f52937de6c521c98cbdeb650
NilsiTop <ntopland@gmail.com>
2019-11-18 21:51:33 +0100

    Updated

--- Caesar Ciper_decrypt_and_encrypt.py
+++ Caesar Ciper_decrypt_and_encrypt.py
@@ -1,5 +1,5 @@
 "Date writen: 17.11.2019"
-"File Name: Caesar Cipher_encypt_and_decrypt.py"
+"File Name: Caesar Cipher_encypt and decrypt.py"
 "Artur: Nils Arne Topland"
 
 
@@ -23,7 +23,7 @@ def mainMeny():
                 mainMeny()
         except ValueError:
             print("Invalid choice, Enter number 1 - 3")
-    exit
+#    exit
 
 
 def encryption():
@@ -46,14 +46,36 @@ def encryption():
                 encryptionmsg += chr(x + 65)
         else:
             encryptionmsg += character
-            print(encryptionmsg)
-            anykey = input("Enter any character to return to main menu:")
-            mainMeny()
+            print("The result is =", encryptionmsg)
+ #       anykey = input("Enter any character to return to main menu:")
+    mainMeny()
+
 
 def Decrypt():
-    print("bad")
-    anykey = input("Enter any character to return to main menu:")
+    Decrypt_message = input("Enter encrypted text :")
+    Decrypt_message = Decrypt_message.lower()
+    shiftValue = int(input("\nEnter a Shift Value : "))
+    encryptionMsg = ""
+
+    for character in Decrypt_message:
+        if character.isalpha() == True:
+            if character == character.lower():
+                x = ord(character) - 97
+                x -= shiftValue
+                x = x % 26
+                encryptionMsg += chr(x + 97)
+            else:
+                x = ord(character) - 65
+                x -= shiftValue
+                x = x % 26
+                encryptionMsg += chr(x + 65)
+        else:
+            encryptionMsg += character
+            print("The result is = ", encryptionMsg)
+
+#        anykey = input("Enter any character to return to main menu:")
     mainMeny()
-#Main
+
+
 mainMeny()
 
